571ab61ca815cce6f56472e7daa00da7ec94bbd6
complikatyed <katye.russell@gmail.com>
2016-10-29 18:18:43 -0500

    First tests of repo and context passing

--- fands2/fands2.Tests/DAL/ResponseRepositoryTest.cs
+++ fands2/fands2.Tests/DAL/ResponseRepositoryTest.cs
@@ -1,14 +1,36 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using fands2.DAL;
+using System.Collections.Generic;
+using fands2.Models;
+using System.Data.Entity;
+using System.Linq;
+
+
+
 
 namespace fands2.Tests.DAL
 {
     [TestClass]
-    public class UnitTest1
+    public class ResponseRepositoryTest
     {
+
         [TestMethod]
-        public void TestMethod1()
+        public void EnsureCanCreateRepoInstance()
         {
+            ResponseRepository repo = new ResponseRepository();
+            Assert.IsNotNull(repo);
+        }
+
+        [TestMethod]
+        public void EnsureRepoHasContext()
+        {
+            ResponseRepository repo = new ResponseRepository();
+
+            ResponseContext actual_context = repo.Context;
+
+            Assert.IsInstanceOfType(actual_context, typeof(ResponseContext));
+
         }
     }
 }
